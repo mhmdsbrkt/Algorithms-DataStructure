5dd4bc4e89ba5d56e0e964104a37baf2a323ebff
Mohammed Barakat <mh.s.barakat@gmail.com>
2015-11-14 13:30:54 +0200

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+
+using namespace std;
+#include <iostream>
+
+int fibonacci(int input) {
+
+    if ((input == 1) || (input == 2)) {
+        return 1;
+    }
+    else {
+        return fibonacci(input - 1) + fibonacci(input - 2);
+    }
+}
+
+int main() {
+    cout << fibonacci(10) << endl;
+    return 0;
+
+}
