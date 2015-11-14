584b96af24cd413efef1abc8c63ed20644245691
Mohammed Barakat <mh.s.barakat@gmail.com>
2015-11-14 13:07:40 +0200

    Update 05-fibo.cpp

--- 05-fibo.cpp
+++ 05-fibo.cpp
@@ -1,15 +1,18 @@
 using namespace std;
 #include <iostream>
 
-int fibo(int input){
+int fibonacci(int input) {
 
-	if ((input == 1) || (input == 2)) { return 1; }
-	else { return fibo(input - 1) + fibo(input - 2); }
-}
-int main()
-{
-	cout << fibo(10) << endl;
-	return 0;
-
+    if ((input == 1) || (input == 2)) {
+        return 1;
+    }
+    else {
+        return fibonacci(input - 1) + fibonacci(input - 2);
+    }
 }
 
+int main() {
+    cout << fibonacci(10) << endl;
+    return 0;
+
+}
