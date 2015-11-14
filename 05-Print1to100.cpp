f496109b419cb28636ad27f6d933fd45e46e3b62
Mohammed Barakat <mh.s.barakat@gmail.com>
2015-11-14 13:05:14 +0200

    Update 05-Print1to100.cpp

--- 05-Print1to100.cpp
+++ 05-Print1to100.cpp
@@ -1,20 +1,15 @@
 using namespace std;
 #include <iostream>
-/* Prints numbers from 1 to n */
-void printNos(unsigned int n)
-{
-  if(n > 0)
-  {
-    printNos(n-1);
-    printf("%d ",  n);
-  }
-  return;
+
+void printNumbers(int n) {
+    if(n > 0) {
+        printNumbers(n - 1);
+      cout << n<<"\t";
+    }
+    return;
 }
 
-/* Driver program to test printNos */
-int main()
-{
-  printNos(100);
-  getchar();
-  return 0;
+int main() {
+    printNumbers(100);
+    return 0;
 }
